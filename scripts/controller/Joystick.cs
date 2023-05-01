dac26ad7e30ffdcb069c184cca0c369e8737244a
Nana <leo.sj.yoon@gmail.com>
2023-05-01 20:03:15 +1000

    9 setup character movement (#11)

--- scripts/controller/Joystick.cs
+++ scripts/controller/Joystick.cs
@@ -3,59 +3,85 @@ using System;
 
 public partial class Joystick : Area2D
 {
-    private Node2D joystick_pad;
-    private Node2D joystick;
-    private float joystick_area_radius;
-    private Vector2 joystick_pad_center;
-    private bool touched = false;
+	[Signal]
+	public delegate Vector2 UseMoveVectorEventHandler();
+	
+	private Node2D joystick_pad;
+	private Node2D joystick;
+	private float joystick_area_radius;
+	private Vector2 joystick_pad_center;
+	private Vector2 move_vector;
+	private Vector2 joystick_radius_limit;
+	public bool touched = false;
 
-    public override void _Ready()
-    {
-        joystick_pad = GetNode<Sprite2D>("joystick_pad");
-        joystick = joystick_pad.GetNode<Sprite2D>("joystick");
-        joystick_area_radius = ((CircleShape2D)GetNode<CollisionShape2D>("joystick_area").Shape).Radius;
-        Hide();
-    }
+	public override void _Ready()
+	{
+		joystick_pad = GetNode<Sprite2D>("joystick_pad");
+		joystick = joystick_pad.GetNode<Sprite2D>("joystick");
+		joystick_area_radius = ((CircleShape2D)GetNode<CollisionShape2D>("joystick_area").Shape).Radius;
+		Hide();
+	}
 
-    public override void _Process(double delta)
-    {
-        if (touched)
-        {
-            float radius = joystick_area_radius; //radius of *black circle*
-            Vector2 center_position = joystick_pad_center; //center of *black circle*
-            float distance = joystick.GetGlobalMousePosition().DistanceTo(center_position); //distance from ~green object~ to *black circle*
+	public override void _Process(double delta)
+	{
+		if (touched)
+		{
+			float distance = joystick.GetGlobalMousePosition().DistanceTo(joystick_pad_center);
+			Vector2 from_origin_to_object = joystick.GetGlobalMousePosition() - joystick_pad_center;
+			from_origin_to_object *= joystick_area_radius / distance;
+			try
+			{
+				joystick_radius_limit = joystick_radius_limit.Clamp(Vector2.Zero, joystick_pad_center + from_origin_to_object);
+			}
+			// Ignore joystick math clamp exception, when the user drags the joystick outside of the gameplay scene.
+			catch
+			{ }
 
-            if (distance > radius) //If the distance is less than the radius, it is already within the circle.
-            {
-                Vector2 from_origin_to_object = joystick.GetGlobalMousePosition() - center_position; //~GreenPosition~ - *BlackCenter*
-                from_origin_to_object *= radius / distance; //Multiply by radius //Divide by Distance
-                joystick.GlobalPosition = center_position + from_origin_to_object; //*BlackCenter* + all that Math
-            }
-            else
-            {
-                joystick.GlobalPosition = GetGlobalMousePosition();
-            }
-        }
-    }
+			if (distance > joystick_area_radius)
+			{
+				joystick.GlobalPosition = joystick_pad_center + from_origin_to_object;
+			}
+			else
+			{
+				joystick.GlobalPosition = joystick.GetGlobalMousePosition();
+			}
+		}
+	}
 
-    public override void _Input(InputEvent @event)
-    {
-        if (@event is InputEventScreenTouch touch)
-        {
-            if (touch.Pressed)
-            {
-                touched = true;
-                joystick_pad.GlobalPosition = GetGlobalMousePosition();
-                joystick_pad_center = joystick_pad.GlobalPosition;
-                Show();
-            }
-            else
-            {
-                touched = false;
-                Hide();
-            }
-        }
-    }
+	public override void _Input(InputEvent @event)
+	{
+		if (@event is InputEventScreenTouch touch)
+		{
+			if (touch.Pressed)
+			{
+				touched = true;
+				joystick_pad.GlobalPosition = GetGlobalMousePosition();
+				joystick_pad_center = joystick_pad.GlobalPosition;
+
+				Show();
+			}
+			else
+			{
+				touched = false;
+				Hide();
+			}
+		}
+
+		if (@event is InputEventScreenTouch touch_event)
+		{
+			move_vector = calculate_move_vector(touch_event.Position);
+
+		}
+		else if (@event is InputEventScreenDrag drag_event)
+		{
+
+			move_vector = calculate_move_vector(drag_event.Position);
+			EmitSignal(SignalName.UseMoveVector, calculate_move_vector(drag_event.Position));
+		}
+	}
+
+	public Vector2 calculate_move_vector(Vector2 event_position)
+	{
+		return (event_position - joystick_pad_center).Normalized();
+	}
 }
-
-
