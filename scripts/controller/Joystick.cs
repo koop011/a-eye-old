a3d47dc9ab39d4de38e1133b34015b65ef09489f
NANA <koop011@hotmail.com>
2023-06-16 14:17:39 +1000

    Modified the avoidance but not sure if it did the job.

--- scripts/controller/Joystick.cs
+++ scripts/controller/Joystick.cs
@@ -3,85 +3,85 @@ using System;
 
 public partial class Joystick : Area2D
 {
-    [Signal]
-    public delegate Vector2 UseMoveVectorEventHandler();
+	[Signal]
+	public delegate Vector2 UseMoveVectorEventHandler();
 
-    private Node2D joystick_pad;
-    private Node2D joystick;
-    private float joystick_area_radius;
-    private Vector2 joystick_pad_center;
-    private Vector2 move_vector;
-    private Vector2 joystick_radius_limit;
-    public bool touched = false;
+	private Node2D joystick_pad;
+	private Node2D joystick;
+	private float joystick_area_radius;
+	private Vector2 joystick_pad_center;
+	private Vector2 move_vector;
+	private Vector2 joystick_radius_limit;
+	public bool touched = false;
 
-    public override void _Ready()
-    {
-        joystick_pad = GetNode<Sprite2D>("joystick_pad");
-        joystick = joystick_pad.GetNode<Sprite2D>("joystick");
-        joystick_area_radius = ((CircleShape2D)GetNode<CollisionShape2D>("joystick_area").Shape).Radius;
-        Hide();
-    }
+	public override void _Ready()
+	{
+		joystick_pad = GetNode<Sprite2D>("joystick_pad");
+		joystick = joystick_pad.GetNode<Sprite2D>("joystick");
+		joystick_area_radius = ((CircleShape2D)GetNode<CollisionShape2D>("joystick_area").Shape).Radius;
+		Hide();
+	}
 
-    public override void _Process(double delta)
-    {
-        if (touched)
-        {
-            float distance = joystick.GetGlobalMousePosition().DistanceTo(joystick_pad_center);
-            Vector2 from_origin_to_object = joystick.GetGlobalMousePosition() - joystick_pad_center;
-            from_origin_to_object *= joystick_area_radius / distance;
-            try
-            {
-                joystick_radius_limit = joystick_radius_limit.Clamp(Vector2.Zero, joystick_pad_center + from_origin_to_object);
-            }
-            // Ignore joystick math clamp exception, when the user drags the joystick outside of the gameplay scene.
-            catch
-            { }
+	public override void _Process(double delta)
+	{
+		if (touched)
+		{
+			float distance = joystick.GetGlobalMousePosition().DistanceTo(joystick_pad_center);
+			Vector2 from_origin_to_object = joystick.GetGlobalMousePosition() - joystick_pad_center;
+			from_origin_to_object *= joystick_area_radius / distance;
+			try
+			{
+				joystick_radius_limit = joystick_radius_limit.Clamp(Vector2.Zero, joystick_pad_center + from_origin_to_object);
+			}
+			// Ignore joystick math clamp exception, when the user drags the joystick outside of the gameplay scene.
+			catch
+			{ }
 
-            if (distance > joystick_area_radius)
-            {
-                joystick.GlobalPosition = joystick_pad_center + from_origin_to_object;
-            }
-            else
-            {
-                joystick.GlobalPosition = joystick.GetGlobalMousePosition();
-            }
-        }
-    }
+			if (distance > joystick_area_radius)
+			{
+				joystick.GlobalPosition = joystick_pad_center + from_origin_to_object;
+			}
+			else
+			{
+				joystick.GlobalPosition = joystick.GetGlobalMousePosition();
+			}
+		}
+	}
 
-    public override void _Input(InputEvent @event)
-    {
-        if (@event is InputEventScreenTouch touch)
-        {
-            if (touch.Pressed)
-            {
-                touched = true;
-                joystick_pad.GlobalPosition = GetGlobalMousePosition();
-                joystick_pad_center = joystick_pad.GlobalPosition;
+	public override void _Input(InputEvent @event)
+	{
+		if (@event is InputEventScreenTouch touch)
+		{
+			if (touch.Pressed)
+			{
+				touched = true;
+				joystick_pad.GlobalPosition = GetGlobalMousePosition();
+				joystick_pad_center = joystick_pad.GlobalPosition;
 
-                Show();
-            }
-            else
-            {
-                touched = false;
-                Hide();
-            }
-        }
+				Show();
+			}
+			else
+			{
+				touched = false;
+				Hide();
+			}
+		}
 
-        if (@event is InputEventScreenTouch touch_event)
-        {
-            move_vector = calculate_move_vector(touch_event.Position);
+		if (@event is InputEventScreenTouch touch_event)
+		{
+			move_vector = calculate_move_vector(touch_event.Position);
 
-        }
-        else if (@event is InputEventScreenDrag drag_event)
-        {
+		}
+		else if (@event is InputEventScreenDrag drag_event)
+		{
 
-            move_vector = calculate_move_vector(drag_event.Position);
-            EmitSignal(SignalName.UseMoveVector, calculate_move_vector(drag_event.Position));
-        }
-    }
+			move_vector = calculate_move_vector(drag_event.Position);
+			EmitSignal(SignalName.UseMoveVector, calculate_move_vector(drag_event.Position));
+		}
+	}
 
-    public Vector2 calculate_move_vector(Vector2 event_position)
-    {
-        return (event_position - joystick_pad_center).Normalized();
-    }
+	public Vector2 calculate_move_vector(Vector2 event_position)
+	{
+		return (event_position - joystick_pad_center).Normalized();
+	}
 }
--- scripts/weapon-controller/Weapon_Controller.cs
+++ scripts/weapon-controller/Weapon_Controller.cs
@@ -4,55 +4,55 @@ using System.Collections.Generic;
 
 public partial class Weapon_Controller : Node2D
 {
-    [Export]
-    public PackedScene ElectricBasicScene { get; set; }
-    private Node2D weaponConsole;
-    private Dictionary<String, bool> weaponArmory = new Dictionary<String, bool>();
-    private player player;
-    private Timer timer;
-    public override void _Ready()
-    {
-        weaponConsole = GetNode<Node2D>("Weapon-Console");
-        player = GetNode<player>("../Player");
-        // weapons list
-        weaponArmory.Add("electric-basic", true);
-        timer = GetNode<Timer>("Timer");
-        timer.WaitTime = 2;
-        timer.Start();
-    }
+	[Export]
+	public PackedScene ElectricBasicScene { get; set; }
+	private Node2D weaponConsole;
+	private Dictionary<String, bool> weaponArmory = new Dictionary<String, bool>();
+	private player player;
+	private Timer timer;
+	public override void _Ready()
+	{
+		weaponConsole = GetNode<Node2D>("Weapon-Console");
+		player = GetNode<player>("../Player");
+		// weapons list
+		weaponArmory.Add("electric-basic", true);
+		timer = GetNode<Timer>("Timer");
+		timer.WaitTime = 2;
+		timer.Start();
+	}
 
-    public override void _Process(double delta)
-    {
+	public override void _Process(double delta)
+	{
 
-    }
+	}
 
-    private void OnTimerTimeout()
-    {
-        if (player.IsEnemyInRange)
-        {
-            WeaponActivation();
-        }
-    }
+	private void OnTimerTimeout()
+	{
+		if (player.IsEnemyInRange)
+		{
+			WeaponActivation();
+		}
+	}
 
-    public void AddWeapon(String name)
-    {
-        weaponArmory[name] = true;
-    }
+	public void AddWeapon(String name)
+	{
+		weaponArmory[name] = true;
+	}
 
-    public Dictionary<String, bool> GetWeaponList()
-    {
-        return weaponArmory;
-    }
+	public Dictionary<String, bool> GetWeaponList()
+	{
+		return weaponArmory;
+	}
 
-    private void WeaponActivation()
-    {
-        // TODO: play scene if weapon is enabled and the scene is not playing.
-        ElectricBasic ElectricBasic = ElectricBasicScene.Instantiate<ElectricBasic>();
+	private void WeaponActivation()
+	{
+		// TODO: play scene if weapon is enabled and the scene is not playing.
+		ElectricBasic ElectricBasic = ElectricBasicScene.Instantiate<ElectricBasic>();
 
-        AddChild(ElectricBasic);
-        //var attack = GetNode<Node2D>("ElectricBasic");
-        //attack.GetNode<Timer>("Timer").Start();
-    }
+		AddChild(ElectricBasic);
+		//var attack = GetNode<Node2D>("ElectricBasic");
+		//attack.GetNode<Timer>("Timer").Start();
+	}
 }
 
 
